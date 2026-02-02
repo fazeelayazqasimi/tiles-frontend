1febacd2e5cfe1fbc6090e579f13231b0c625100
Fazeel Ayaz Qasimi <154905970+fazeelayazqasimi@users.noreply.github.com>
2026-02-02 05:00:16 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // agar aap custom domain ya subfolder me host kar rahe ho, to base set karna
   base: '/',
   server: {
-    port: 5173, // local development port
-    strictPort: true,
+    port: 5173,
+    strictPort: true
   },
   build: {
-    outDir: 'dist', // default Vite build folder
-  },
+    outDir: 'dist'
+  }
 });
